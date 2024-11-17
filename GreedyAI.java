a2040c9f84d0b6c3b818301ace1b008aa341f2bd
adi-wayn <adiwayn@gmail.com>
2024-11-17 20:06:05 +0200

    17.11.24

--- GreedyAI.java
+++ GreedyAI.java
@@ -1,6 +1,6 @@
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
-import java.util.Random;
 
 public class GreedyAI extends AIPlayer {
 
@@ -10,51 +10,35 @@ public class GreedyAI extends AIPlayer {
 
     private List<Position> topMoves(PlayableLogic gameStatus, List<Position> positions) {
         List<Position> topMoves = new ArrayList<>();
-        int maxFlip = maxFlip(gameStatus, positions);
+        int maxFlip = gameStatus.countFlips(positions.getLast());
 
-        for (Position position : positions) {
-            if (gameStatus.countFlips(position) == maxFlip)
-                topMoves.add(position);
+        for (int i = positions.size()-1 ; i >= 0 && gameStatus.countFlips(positions.get(i)) == maxFlip ; i--) {
+            topMoves.add(positions.get(i));
         }
 
         return topMoves;
     }
 
-    private int maxFlip(PlayableLogic gameStatus, List<Position> positions) {
-        int maxFlip = 0;
-
-        for (Position position : positions) {
-            int candidate = gameStatus.countFlips(position);
-            if (candidate > maxFlip) {
-                maxFlip = candidate;
-            }
-        }
-
-        return maxFlip;
-    }
-
     private List<Position> topRightMoves(List<Position> topMoves) {
         List<Position> topRightMoves = new ArrayList<>();
-        int rightCol = rightCol(topMoves);
+        int rightCol = topMoves.getLast().col();
 
-        for (Position position : topMoves) {
-            if (position.col() == rightCol)
-                topRightMoves.add(position);
+        for (int i = topMoves.size()-1 ; i >= 0 && topMoves.get(i).col() == rightCol ; i--) {
+            topRightMoves.add(topMoves.get(i));
         }
 
         return topRightMoves;
     }
 
-    private int rightCol(List<Position> topMoves) {
-        int rightCol = 0;
+    private List<Position> topBottomMoves(List<Position> topRightMoves) {
+        List<Position> topBottomMoves = new ArrayList<>();
+        int bottomRow = topRightMoves.getLast().row();
 
-        for (Position position : topMoves) {
-            if (position.col() > rightCol) {
-                rightCol = position.col();
-            }
+        for (int i = topRightMoves.size()-1 ; i >= 0 && topRightMoves.get(i).row() == bottomRow ; i--) {
+            topBottomMoves.add(topRightMoves.get(i));
         }
 
-        return rightCol;
+        return topBottomMoves;
     }
 
     @Override
@@ -63,22 +47,20 @@ public class GreedyAI extends AIPlayer {
         Player player = gameStatus.isFirstPlayerTurn() ? gameStatus.getFirstPlayer() : gameStatus.getSecondPlayer();
         List<Position> positions = gameStatus.ValidMoves();
 
+        positions.sort(Comparator.comparingInt(gameStatus::countFlips));
         List<Position> topMoves = topMoves(gameStatus, positions);
-        Position bestPos = topMoves.getFirst();
+        Position bestPos = topMoves.getLast();
 
 
         if (topMoves.size() > 1) {
+            topMoves.sort(Comparator.comparingInt(Position::col));
             List<Position> topRightMoves = topRightMoves(topMoves);
-            bestPos = topRightMoves.getFirst();
+            bestPos = topRightMoves.getLast();
 
             if (topRightMoves.size() > 1) {
-                int bottomRow = 0;
-                for (Position position : topRightMoves) {
-                    if (position.row() > bottomRow) {
-                        bottomRow = position.row();
-                        bestPos = position;
-                    }
-                }
+                topRightMoves.sort(Comparator.comparingInt(Position::row));
+                List<Position> topBottomMoves = topBottomMoves(topRightMoves);
+                bestPos = topBottomMoves.getLast();
             }
         }
 
